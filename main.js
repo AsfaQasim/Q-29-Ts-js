1ef2fc901c3cda162fc0e0483de82207a73bf41c
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-08 08:47:10 -0800

    Q 29 of Ts/js & node js of 45 assignment.
    Q 29 of Ts/js & node js of 45 assignment.

---
+++ main.js
@@ -0,0 +1,10 @@
+var favoriteFruits = ["Apple", "Banana", "Orange"];
+if (favoriteFruits.includes("Mango")) {
+    console.log("I would like to eat Mango");
+}
+else if (favoriteFruits.includes("Apple")) {
+    console.log("I would like to eat Apple");
+}
+else {
+    console.log("fruits are not available!");
+}
---
+++ main.ts
@@ -0,0 +1,11 @@
+let favoriteFruits = ["Apple","Banana","Orange"];
+
+if(favoriteFruits.includes("Mango")){
+    console.log(`I would like to eat Mango`);
+
+}else if(favoriteFruits.includes("Apple")){
+    console.log(`I would like to eat Apple`);
+
+}else{
+    console.log(`fruits are not available!`);
+}
